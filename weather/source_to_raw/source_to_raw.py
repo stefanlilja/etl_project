5134e1ca9b2de21ebd10c9f14b4f4120303a0336
stefanlilja <stefanlilja@brightstraining.com>
2023-01-18 11:21:14 +0100

    modified source_to_raw.py to fetch weather data

--- weather/source_to_raw/source_to_raw.py
+++ weather/source_to_raw/source_to_raw.py
@@ -7,17 +7,19 @@ current_dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(current_dir_path, os.pardir))
 
 def request_from_url(url):
-    r = requests.get(url)
+    r = requests.get(url, headers={'User-Agent': 'TheSebastians'})
     return r.json()
 
 def write_to_file(dict):
     
-    with open(parent_dir_path + '\\data\\testing\\raw\\data.json', 'w') as file:
+    with open(parent_dir_path + '\\data\\weather\\raw\\data.json', 'w') as file:
         json.dump(dict, file)
 
 
 test_url = 'http://swapi.dev/api/planets/1/'
 
-json_data = request_from_url(test_url)
+cape_town_url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=-34&lon=18'
+
+json_data = request_from_url(cape_town_url)
 
 write_to_file(json_data)
